7c948bdd71dab5e7f165154f0229acaed7ef9613
FerrariLuca04 <lucaferrariroggio@gmail.com>
2026-08-06 21:17:28 +0200

    Initial project structure

---
+++ README.md
@@ -0,0 +1,2 @@
+Educational Geant4 simulation inspired by the NA62 RICH detector. This project is not official NA62 software.
+
---
+++ app/main.cc
@@ -0,0 +1,9 @@
+#include "G4ios.hh"
+
+int main()
+{
+    G4cout << "NA62RICHSim started successfully." << G4endl;
+    G4cout << "Geant4 compilation and linking work." << G4endl;
+
+    return 0;
+}
